80fb24d5c9d12dff19202485ffdcc6bf6fc40cae
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-05-31 15:17:10 +0600

    updated

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$('.w-slide-inner').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '.me-prev',
+    nextArrow: '.me-next',
+  });
+$('.w-slide-inner1').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '.me-prev1',
+    nextArrow: '.me-next2',
+  });
+$('.ban_vidslider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '.me-prev3',
+    nextArrow: '.me-next4',
+  });
