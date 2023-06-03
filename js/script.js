60083d14dd620354d137b297a52da562a26b0efb
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-06-03 15:29:53 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -20,3 +20,22 @@ $('.ban_vidslider').slick({
     prevArrow: '.me-prev3',
     nextArrow: '.me-next4',
   });
+$('.item-slider').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:5000,
+    asNavFor:'.text-slider',
+  });
+
+  $('.text-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor:'.item-slider',
+    prevArrow: '.asnavbtn i.fa-chevron-left',
+    nextArrow: '.asnavbtn i.fa-chevron-right',
+  });
